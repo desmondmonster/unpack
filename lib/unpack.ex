6cd0430a1e643034b6a330ee4ff8c7236e4a2e18
Doug Wright <mrdougwright@gmail.com>
2018-10-01 18:53:39 -0700

    try protocol

--- lib/unpack.ex
+++ lib/unpack.ex
@@ -1,10 +1,9 @@
-defmodule Unpack do
+defprotocol Unpack do
+  @fallback_to_any true
   @moduledoc """
   Unpack lets you "unpack" values from a nested map safely.
-
-  It's a very lightweight module that expects you to have
-  an Ecto dependency.
   """
+
   @doc """
   Traverses any nested map or `struct`, in order of `keys` list, to return a
   value. Returns nil for bad keys, unloaded ecto associations, or empty maps.
@@ -18,7 +17,14 @@ defmodule Unpack do
       iex> Unpack.get(struct, [:player, :game, :id])
       nil
   """
+  def get(data, keys)
+end
+
+defimpl Unpack, for: Ecto.Association.NotLoaded do
   def get(%Ecto.Association.NotLoaded{}, _), do: nil
+end
+
+defimpl Unpack, for: Any do
   def get(data, []), do: data
   def get(data, [key | tail]) when is_map(data), do:
     get(Map.get(data, key), tail)
